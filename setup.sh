7327d92a7db5bc1c325fe0018943f85d983db698
Robert Tirrell <rpt@solumtech.com>
2011-06-17 21:38:24 -0700

    Slight and final fix.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,20 @@
+#!/bin/bash
 if [ ! -f ~/.bashrc ]; then
     echo "source ~/.rpt_profile/etc/bashrc" >> ~/.bashrc
 else 
     if [ $(grep .rpt_profile ~/.bashrc | wc -l ) -eq 0 ]; then
         echo "source ~/.rpt_profile/etc/bashrc" >> ~/.bashrc
-        echo "set -o vi" >> ~/.bashrc
     fi
 fi
 
 echo "set editing-mode vi" > ~/.inputrc
 echo "set keymap vi" >> ~/.inputrc
 
-echo "source $HOME/.rpt_profile/etc/screenrc" >> ~/.screenrc
-
-if [ ! -f ~/.vimrc ]; then
-    echo "source ~/.rpt_profile/etc/vimrc" >> ~/.vimrc
-fi
+echo "source $HOME/.rpt_profile/etc/screenrc" > ~/.screenrc
+echo "source ~/.rpt_profile/etc/vimrc" > ~/.vimrc
 
 if [ -f ~/.bashrc ]; then
     source ~/.bashrc
-fi;
+fi
 
 

f68a8697c8f098ac8b8b06194b13bdb08d46e495
Robert Tirrell <r.tirrell@gmail.com>
2011-11-28 12:26:23 -0800

    New ignores in pylint, better tabbing in vim.

--- etc/bashrc.sh
+++ etc/bashrc.sh
@@ -2,8 +2,9 @@ set -o vi
 export EDITOR="vim"
 export PS1="[\[\e[28;1m\]\h \[\e[0m\]\W]\$ "
 
+# Launch screen.
 s() {
-  if [ -d $1 ]; then
+  if [[ -d $1 ]]; then
     cd $1
     screen -SDR $(basename $(pwd))
   else
@@ -11,26 +12,38 @@ s() {
   fi;
 }
 
+# grep, ignoring anything in .svn.
 grepws() {
     grep -Ir $1 $2 | grep -v .svn
 }
 
-rmsvn() {
+# rm all .svn dirs.
+rm_svn() {
     find . -name ".svn" -type d -exec rm -rf {} \;
 }
 
-dochildren() {
+# Do something to all directories immediately in the current one.
+do_children() {
     find . -maxdepth 1 -mindepth 1 -type d -exec $1 {} \;
 }
 
 svnupall() {
-    dochildren 'svn up'
+    do_children 'svn up'
 }
 
 svnstall() {
-    dochildren 'svn st'
+    do_children 'svn st'
 }
 
 
 for f in $(find ~/.rpt_profile/etc/sites -type f); do source $f; done
 source ~/.rpt_profile/etc/bash_aliases.sh
+
+# Use bash completion on Macports. Should already be installed on Linux.
+if [ -f /opt/local/etc/bash_completion ]; then
+    source /opt/local/etc/bash_completion
+else
+    if [[ `uname` == "Darwin" ]]; then
+        echo "bash-completion is not installed!"
+    fi
+fi

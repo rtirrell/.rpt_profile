22b1fbb3b527708326f165a31299e3bd76ff10ff
Rob Tirrell <r.tirrell@gmail.com>
2012-02-01 14:02:17 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cd
 wget https://github.com/rtirrell/.rpt_profile/tarball/master
 
 tar -xzf master
-rm -fr master
+rm -fr master .rpt_profile
 mv rtirrell* .rpt_profile
 
 for fn in bashrc inputrc screenrc pylintrc vimrc.before vimrc.after; do 
@@ -14,6 +14,7 @@ for fn in bashrc inputrc screenrc pylintrc vimrc.before vimrc.after; do
                 echo "source ~/.rpt_profile/etc/bashrc" >> ~/.bashrc
             fi
         elif [[ -f ~/.rpt_profile/etc/$fn ]]; then
+            echo "Copying $f..."
             ln -sf ~/.rpt_profile/etc/$fn ~/.$fn
         fi
     fi

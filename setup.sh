b6a03ec63ffa93791781eb2744286d206ce2aa11
Robert Tirrell <r.tirrell@gmail.com>
2012-06-27 21:02:59 -0700

    Separate into setup and update.

--- setup.sh
+++ setup.sh
@@ -16,21 +16,4 @@ rm -fr master
 # Named by username, plus repo name and a bunch of junk.
 mv rtirrell* .rpt_profile
 
-
-if [[ $(grep rpt_profile ~/.bashrc | wc -l) == 0 ]]; then
-    echo 'source ~/.rpt_profile/etc/bashrc' >> ~/.bashrc
-fi
-
-filenames=screenrc pylintrc vimrc.before vimrc.after
-for filename in $filenames; do
-    ln -sf ~/.rpt_profile/etc/$filename ~/.$filename
-done
-
-pip install --user flake8 pylint ipython
-source ~/.bashrc
-
-curl -Lo- https://bit.ly/janus-bootstrap | bash
-
-if [[ -f ~/.vim/Rakefile ]]; then
-    cd ~/.vim && rake
-fi
+bash update.sh

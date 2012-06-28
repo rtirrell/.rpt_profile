6c8e0552f4fbd27b23cf739e8cac6a8296e01855
Robert Tirrell <r.tirrell@gmail.com>
2012-06-27 21:00:19 -0700

    Allow overriding of homebase check.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-if [[ $(hostname | grep dazzle) ]]; then
+if [[ $(hostname | grep dazzle) && ! $FORCE_SETUP ]]; then
     echo "Should not be run from dazzle, will kill local changes."
     exit 1
 fi
 
 cd
-wget --no-check-certificate https://github.com/rtirrell/.rpt_profile/tarball/master
+mv .rpt_profile .rpt_profile.bak
+
+wget --no-check-certificate \
+    https://github.com/rtirrell/.rpt_profile/tarball/master
+
 tar -xzf master
 rm -fr master
-rm -fr .rpt_profile
+
 # Named by username, plus repo name and a bunch of junk.
 mv rtirrell* .rpt_profile
 
 
 if [[ $(grep rpt_profile ~/.bashrc | wc -l) == 0 ]]; then
-    echo "source ~/.rpt_profile/etc/bashrc" >> ~/.bashrc
+    echo 'source ~/.rpt_profile/etc/bashrc' >> ~/.bashrc
 fi
 
-filenames="screenrc pylintrc vimrc.before vimrc.after"
+filenames=screenrc pylintrc vimrc.before vimrc.after
 for filename in $filenames; do
     ln -sf ~/.rpt_profile/etc/$filename ~/.$filename
 done

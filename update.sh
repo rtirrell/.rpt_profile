1db60f5bc37a181d3b7aa9bf95959f84ed3d5189
Robert Tirrell <r.tirrell@gmail.com>
2012-06-30 16:54:11 -0700

    Fix relative paths.

--- update.sh
+++ update.sh
@@ -2,13 +2,13 @@ if [[ $(grep rpt_profile ~rpt/.bashrc | wc -l) == 0 ]]; then
     echo 'source ~rpt/.rpt_profile/etc/bashrc' >> ~rpt/.bashrc
 fi
 
-filenames='screenrc pylintrc vimrc.before vimrc.after'
+filenames=screenrc pylintrc vimrc.before vimrc.after
 for filename in $filenames; do
     ln -sf ~rpt/.rpt_profile/etc/$filename ~rpt/.$filename
 done
 
 mkdir -p ~/.byobu
-for f in $(ls ~rpt/.rpt_profile/etc/byobu); do
+for f in $(find ~rpt/.rpt_profile/etc/byobu); do
     cp $f ~rpt/.byobu
 done
 

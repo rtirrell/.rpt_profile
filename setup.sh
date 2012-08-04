4c84a55d88ca9ffec217add20621c93f9a205ca7
Rob Tirrell <r.tirrell@gmail.com>
2012-08-04 14:20:13 -0500

    Pathing - again.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Setup for use.
 
-if [[ -e $HOME/.rpt_profile/.git ]]; then
+if [[ $(pwd) != $HOME ]]; then
+    echo "Run from $HOME."
+fi
+
+if [[ -e .rpt_profile/.git ]]; then
     echo "Should not be run on a system with a repository."
     exit 1
 fi
 
-if [[ -d $HOME/.rpt_profile ]]; then
-    rm -fr $HOME/.rpt_profile.bak
-    mv $HOME/.rpt_profile $HOME/.rpt_profile.bak
+if [[ -d .rpt_profile ]]; then
+    rm -fr .rpt_profile.bak
+    mv .rpt_profile .rpt_profile.bak
 fi
 
 wget --no-check-certificate \
@@ -18,7 +22,7 @@ tar -xzf master
 rm -fr master
 
 # Named by username, plus repo name and a bunch of junk (commit ID, etc.).
-mv rtirrell* $HOME/.rpt_profile
+mv rtirrell* .rpt_profile
 
 curl -Lo- https://bit.ly/janus-bootstrap | bash
-$HOME/.rpt_profile/install.sh
+.rpt_profile/install.sh
